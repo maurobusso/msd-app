87a9e51cec25ad4fc24c2a641c4d9beaf75a3e12
Mauro Busso <mauro.busso12gmail.com>
2023-02-13 20:58:04 +0000

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,10 @@ module.exports = {
   ],
   theme: {
     screens: {
-      'lg': '1024px'
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
     },
     extend: {},
   },
