d14ea4a99edd01c139e06a5c9137a7cd48a13ddb
Mauro Busso <mauro.busso12gmail.com>
2023-02-16 20:27:53 +0000

    changes to alerts

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const drugName = document.getElementById('drugName')
 const msdForKilogram = document.getElementById('msdForKilogram')
 const cartrigeSize = document.getElementById('cartrigeSize')
 const results = document.getElementById('results')
-const attention = document.getElementById('attention')
+// const attention = document.getElementById('attention')
 const femaleCheckbox = document.getElementById('female')
 const maximumSafeDosage = document.getElementById('maximumSafeDosage')
 
